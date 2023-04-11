fea6c84c151f697c15ac232bf0c42700648c4c2d
omgshalihin <shalihinsgp@gmail.com>
2023-04-11 17:20:25 +0200

    value annotation for SECRET

--- src/main/java/com/example/twitapi/security/service/JWTService.java
+++ src/main/java/com/example/twitapi/security/service/JWTService.java
@@ -1,11 +1,11 @@
 package com.example.twitapi.security.service;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -20,11 +20,13 @@ import java.util.function.Function;
 @Service
 public class JWTService {
 
+    @Value("${jwt.secret}")
+    private String SECRET;
+
     public String generateToken(String username) {
         Map<String, Object> claims = new HashMap<>();
         return createToken(claims, username);
     }
-
     private String createToken(Map<String, Object> claims, String username) {
         return Jwts.builder()
                 .setClaims(claims)
@@ -34,27 +36,20 @@ public class JWTService {
                 .signWith(getSignKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
-
     private Key getSignKey() {
-        Dotenv dotenv = null;
-        dotenv = Dotenv.configure().load();
-        byte[] keyBytes = Decoders.BASE64.decode(dotenv.get("JWT_256_BIT_SECRET"));
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
         return Keys.hmacShaKeyFor(keyBytes);
     }
-
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
     }
-
     public Date extractExpiration(String token) {
         return extractClaim(token, Claims::getExpiration);
     }
-
     private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
     }
-
     private Claims extractAllClaims(String token) {
         return Jwts
                 .parserBuilder()
@@ -63,11 +58,9 @@ public class JWTService {
                 .parseClaimsJws(token)
                 .getBody();
     }
-
     private Boolean isTokenExpired(String token) {
         return extractExpiration(token).before(new Date());
     }
-
     public Boolean validateToken(String token, UserDetails userDetails) {
         final  String username = extractUsername(token);
         return (username.equals(userDetails.getUsername()) &&!isTokenExpired(token));
